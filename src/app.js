9eebd7026fb541507ffb6572ba8d04908143adfe
Klaudine <109423321+klaudine@users.noreply.github.com>
2023-10-27 21:58:42 +0000

    updated features

--- src/app.js
+++ src/app.js
@@ -28,12 +28,31 @@ let getSuit = () => {
   return suit[randomSuit];
 };
 
+let getSuitBottom = () => {
+  let suit = ["♦", "♠", "♣", "♥"];
+  let randomSuit = Math.floor(Math.random() * suit.length);
+  return suit[randomSuit];
+};
+
 let randomTopSuit = getSuit();
+// let randomBottomSuit = getSuit();
 
 window.onload = function() {
-  document.querySelector(".top-suit").innerHTML = randomTopSuit;
+  var topSuit = (document.querySelector(".top-suit").innerHTML = randomTopSuit);
+  var bottomSuit = (document.querySelector(
+    ".bottom-suit"
+  ).innerHTML = randomTopSuit);
   document.querySelector(".number").innerHTML = getCard();
-  document.querySelector(".bottom-suit").innerHTML = randomTopSuit;
+
+  if (
+    (topSuit == "♥" && bottomSuit == "♥") ||
+    (topSuit == "♦" && bottomSuit == "♦")
+  ) {
+    var topSuit = (document.querySelector(".top-suit").style.color = "red");
+    var bottomSuit = (document.querySelector(".bottom-suit").style.color =
+      "red");
+    document.querySelector(".number").style.color = "red";
+  }
 };
 
 let refreshPage = function() {
@@ -48,10 +67,26 @@ refreshButton.addEventListener("click", refreshPage);
 
 // // Allow the user to specify the card width and height using text inputs
 // // Get input from user to change height and width of card
-let userDimensions = () => {};
+// let userDimensions = () => {};
 
 const userWidth = document.getElementsByClassName("width");
-userInput.addEventListener("input", () => {});
+userInput.addEventListener("input", () => {
+  console.log();
+});
 
 const userHeight = document.getElementsByClassName("height");
-userInput.addEventListener("input", () => {});
+userInput.addEventListener("input", () => {
+  console.log();
+});
+
+document
+  .querySelector("#userInput")
+  .addEventListener("submit", function(event) {
+    const inputWidth = document.getElementById("width").value;
+    console.log(inputWidth);
+    const inputHeight = document.getElementById("height").value;
+    console.log(inputHeight);
+    const cardBody = document.querySelector(".card");
+    cardBody.style.width = inputWidth + "px";
+    cardBody.style.height = inputHeight + "px";
+  });
