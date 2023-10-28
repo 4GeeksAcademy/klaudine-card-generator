55df520acdf5c82c4b776845cef952c3bdb6595e
Klaudine <109423321+klaudine@users.noreply.github.com>
2023-10-28 01:00:25 +0000

    new changes

--- src/app.js
+++ src/app.js
@@ -28,14 +28,7 @@ let getSuit = () => {
   return suit[randomSuit];
 };
 
-let getSuitBottom = () => {
-  let suit = ["♦", "♠", "♣", "♥"];
-  let randomSuit = Math.floor(Math.random() * suit.length);
-  return suit[randomSuit];
-};
-
 let randomTopSuit = getSuit();
-// let randomBottomSuit = getSuit();
 
 window.onload = function() {
   var topSuit = (document.querySelector(".top-suit").innerHTML = randomTopSuit);
@@ -65,27 +58,12 @@ refreshButton.addEventListener("click", refreshPage);
 
 // setInterval(refreshPage, 10000);
 
-// // Allow the user to specify the card width and height using text inputs
-// // Get input from user to change height and width of card
-// let userDimensions = () => {};
-
-const userWidth = document.getElementsByClassName("width");
-userInput.addEventListener("input", () => {
-  console.log();
-});
-
-const userHeight = document.getElementsByClassName("height");
-userInput.addEventListener("input", () => {
-  console.log();
-});
-
 document
   .querySelector("#userInput")
   .addEventListener("submit", function(event) {
+    event.preventDefault();
     const inputWidth = document.getElementById("width").value;
-    console.log(inputWidth);
     const inputHeight = document.getElementById("height").value;
-    console.log(inputHeight);
     const cardBody = document.querySelector(".card");
     cardBody.style.width = inputWidth + "px";
     cardBody.style.height = inputHeight + "px";
